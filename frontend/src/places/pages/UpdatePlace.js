6a0bd55777623968ab75e26d25022c1e477de2c6
Igor Kruzhynskyi <ikruzhynskyy@gmail.com>
2022-11-19 18:13:21 +0200

    create new update place component and work with it

---
+++ frontend/src/places/pages/UpdatePlace.js
@@ -0,0 +1,95 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+import {
+  VALIDATOR_REQUIRE,
+  VALIDATOR_MINLENGTH,
+} from "../../shared/util/validators";
+import Input from "../../shared/components/FormElements/Input";
+import Button from "../../shared/components/FormElements/Button";
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    imageUrl:
+      "https://www.history.com/.image/t_share/MTU3ODc4NjA0ODYzOTA3NTUx/image-placeholder-title.jpg",
+    title: "Empire State Building",
+    description: "The most famous place in NY",
+    address: "20 W 34th St., New York",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9856644,
+    },
+    creator: "u1",
+  },
+  {
+    id: "p2",
+    imageUrl:
+      "https://www.history.com/.image/t_share/MTU3ODc4NjA0ODYzOTA3NTUx/image-placeholder-title.jpg",
+    title: "Empire State Building",
+    description: "The most famous place in NY",
+    address: "20 W 34th St., New York",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9856644,
+    },
+    creator: "u2",
+  },
+  {
+    id: "p3",
+    imageUrl:
+      "https://www.history.com/.image/t_share/MTU3ODc4NjA0ODYzOTA3NTUx/image-placeholder-title.jpg",
+    title: "Empire State Building",
+    description: "The most famous place in NY",
+    address: "20 W 34th St., New York",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9856644,
+    },
+    creator: "u3",
+  },
+];
+
+const UpdatePlace = () => {
+  const placeId = useParams().placeId;
+
+  const identifiedPlace = DUMMY_PLACES.find((p) => p.id === placeId);
+
+  if (!identifiedPlace) {
+    return (
+      <div className="center">
+        <h2>Could not find place</h2>
+      </div>
+    );
+  }
+  return (
+    <form>
+      <Input
+        id="title"
+        element="input"
+        type="text"
+        label="Title"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid title"
+        onInput={() => {}}
+        value={identifiedPlace.title}
+        valid={true}
+      />
+      <Input
+        id="description"
+        element="textarea"
+        label="Description"
+        validators={[VALIDATOR_MINLENGTH()]}
+        errorText="Please enter a valid description (min. 5 characters)"
+        onInput={() => {}}
+        value={identifiedPlace.description}
+        valid={true}
+      />
+      <Button type="submit" disabled={true}>
+        UPDATE PLACE
+      </Button>
+    </form>
+  );
+};
+
+export default UpdatePlace;

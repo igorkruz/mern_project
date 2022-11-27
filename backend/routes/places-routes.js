f1412e1aae4577b80f5cd2affdcf2d3dcaff19ff
Igor Kruzhynskyi <ikruzhynskyy@gmail.com>
2022-11-27 19:19:20 +0200

    added place-specific routes

--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -2,9 +2,28 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  console.log("GET requeset in places");
-  res.json({ message: "It works!" });
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    // imageUrl:
+    //   "https://www.history.com/.image/t_share/MTU3ODc4NjA0ODYzOTA3NTUx/image-placeholder-title.jpg",
+    title: "Empire State Building",
+    description: "The most famous place in NY",
+    address: "20 W 34th St., New York",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9856644,
+    },
+    creator: "u1",
+  },
+];
+
+router.get("/:pid", (req, res, next) => {
+  const placeId = req.params.pid; //  {pid: p1}
+  const place = DUMMY_PLACES.find((p) => {
+    return p.id === placeId;
+  });
+  res.json({ place }); // {place} => {place: place}
 });
 
 module.exports = router;

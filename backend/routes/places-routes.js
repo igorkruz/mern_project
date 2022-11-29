4cda6d32a3b0c40569c5fab98fa6550571590402
Igor Kruzhynskyi <ikruzhynskyy@gmail.com>
2022-11-29 15:27:48 +0200

    added own error model

--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -1,5 +1,7 @@
 const express = require("express");
 
+const HttpError = require("../models/http-error");
+
 const router = express.Router();
 
 const DUMMY_PLACES = [
@@ -26,9 +28,7 @@ router.get("/:pid", (req, res, next) => {
   });
 
   if (!place) {
-    const error = new Error("Could not find place for a provided id.");
-    error.code = 404;
-    throw error;
+    throw new HttpError("Could not find place for a provided id.", 404);
   }
 
   res.json({ place }); // {place} => {place: place}
@@ -42,9 +42,9 @@ router.get("/user/:uid", (req, res, next) => {
   });
 
   if (!place) {
-    const error = new Error("Could not find place for a provided user id.");
-    error.code = 404;
-    return next(error);
+    return next(
+      new HttpError("Could not find place for a provided user id.", 404)
+    );
   }
 
   res.json({ place });

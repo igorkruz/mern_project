ee039c3c954836d31ea47f469050d218b0b87629
Igor Kruzhynskyi <ikruzhynskyy@gmail.com>
2022-12-02 13:46:36 +0200

    add verefication if email exist

--- backend/controllers/users-controllers.js
+++ backend/controllers/users-controllers.js
@@ -18,6 +18,12 @@ const getUsers = (req, res, next) => {
 const signup = (req, res, next) => {
   const { name, email, password } = req.body;
 
+  const hasUser = DUMMY_USERS.find((u) => u.email === email);
+
+  if (hasUser) {
+    throw new HttpError("Could not create user, email already exist", 422);
+  }
+
   const createdUser = {
     id: uuid,
     name,

e0d204b3b5cc6920437df8fb2b844c9220754327
Igor Kruzhynskyi <ikruzhynskyy@gmail.com>
2022-12-05 13:16:07 +0200

    get places  by the placesID

--- backend/controllers/places-controllers.js
+++ backend/controllers/places-controllers.js
@@ -21,18 +21,26 @@ let DUMMY_PLACES = [
   },
 ];
 
-const getPlacesById = (req, res, next) => {
+const getPlacesById = async (req, res, next) => {
   const placeId = req.params.pid; //  {pid: p1}
 
-  const place = DUMMY_PLACES.find((p) => {
-    return p.id === placeId;
-  });
-
-  if (!place) {
-    throw new HttpError("Could not find place for a provided id.", 404);
+  let place;
+  try {
+    place = await Place.findById(placeId);
+  } catch (err) {
+    const error = new HttpError(
+      "Somthing went wrong, could not find a place ",
+      500
+    );
+    return next(error);
   }
 
-  res.json({ place }); // {place} => {place: place}
+  if (!place) {
+    const error = new HttpError("Could not find place for a provided id.", 404);
+    return next(error);
+  }
+
+  res.json({ place: place.toObject({ getters: true }) }); // {place} => {place: place}
 };
 
 const getPlacesByUserId = (req, res, next) => {

1e2875911c9d0273c9f6687fdd89b090013f6d4a
Igor Kruzhynskyi <ikruzhynskyy@gmail.com>
2022-12-09 15:44:39 +0200

    working with place-controllers

--- backend/controllers/places-controllers.js
+++ backend/controllers/places-controllers.js
@@ -2,8 +2,10 @@ const { v4: uuidv4 } = require("uuid");
 const { validationResult } = require("express-validator");
 
 const Place = require("../models/place");
+const User = require("../models/user");
 const HttpError = require("../models/http-error");
 const getCoordsByAdress = require("../util/location");
+const { default: mongoose } = require("mongoose");
 
 let DUMMY_PLACES = [
   {
@@ -98,8 +100,28 @@ const createPlace = async (req, res, next) => {
     creator,
   });
 
+  let user;
   try {
-    await createdPlace.save();
+    user = await User.findById(creator);
+  } catch (err) {
+    const error = new HttpError("Creating place failed, please try again", 500);
+    return next(error);
+  }
+
+  if (!user) {
+    const error = new HttpError("Could not find user for provided ID", 404);
+    return next(error);
+  }
+
+  console.log(user);
+
+  try {
+    const sess = await mongoose.startSession();
+    sess.startTransaction();
+    await createdPlace.save({ session: sess });
+    user.places.push(createdPlace);
+    await user.save({ session: sess });
+    await sess.commitTransaction();
   } catch (err) {
     const error = new HttpError("Creating place failde, plase try again", 500);
     return next(error);

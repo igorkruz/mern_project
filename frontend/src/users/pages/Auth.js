5db73dd690da53407ba183098f3e643e4d5a107d
Igor Kruzhynskyi <ikruzhynskyy@gmail.com>
2022-12-13 10:31:06 +0200

    create logic to send request to backend from frontend for signup user

--- frontend/src/users/pages/Auth.js
+++ frontend/src/users/pages/Auth.js
@@ -58,9 +58,31 @@ const Authentefication = () => {
     setIsLoginMode((prev) => !prev);
   };
 
-  const authSubmitHandler = (event) => {
+  const authSubmitHandler = async (event) => {
     event.preventDefault();
-    console.log(formState);
+
+    if (isLoginMode) {
+    } else {
+      try {
+        const response = await fetch("http://localhost:5000/api/users/signup", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            name: formState.inputs.name.value,
+            email: formState.inputs.email.value,
+            password: formState.inputs.password.value,
+          }),
+        });
+
+        const responseData = await response.json();
+        console.log(responseData);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+
     auth.login();
   };
 
